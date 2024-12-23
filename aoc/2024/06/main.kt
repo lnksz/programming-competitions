f82b93ae63b577ef52ed188de09d2dcd55b780e8
Kristóf Havasi <lnksz@users.noreply.github.com>
2024-12-23 20:03:50 +0000

    Day 6/2
    Watch where you step!

--- aoc/2024/06/main.kt
+++ aoc/2024/06/main.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.time.LocalTime
 
 enum class Dir {
     N, E, S, W;
@@ -6,103 +7,111 @@ enum class Dir {
         return values()[(ordinal + 1) % values().size]
     }
 }
+
 data class Point(
     val x: Int,
     val y: Int,
 )
+
 data class Vector(
     val p: Point,
     val d: Dir,
 )
-data class FloorMap(
-    val xhi: Int,
-    val yhi: Int,
-    val obsts: List<Point>,
-    val guy: Vector,
-)
 
+data class FloorMap(
+    val xlim: Int,
+    val ylim: Int,
+    val obsts: List<Point>,
+    val start: Vector,
+) {
+    fun isInside(p: Point): Boolean {
+        return p.x in 0 until xlim && p.y in 0 until ylim
+    }
+
+    fun print(visited: Set<Vector> = emptySet()) {
+        for (y in 0 until ylim) {
+            for (x in 0 until xlim) {
+                val p = Point(x, y)
+                val char = when {
+                    obsts.contains(p) -> '#'
+                    start.p == p -> 'S'
+                    visited.any { it.p == p } -> '*'
+                    else -> '.'
+                }
+                print(char)
+            }
+            println()
+        }
+    }
+
+    fun calcNext(v: Vector): Vector {
+        val n = v.step()
+        return if (obsts.contains(n.p)) {
+            Vector(v.p, v.d.turn())
+        } else {
+            n
+        }
+    }
+
+    fun isLoopy(): Boolean {
+        var pos = start
+        val visited = mutableSetOf<Vector>()
+        while (isInside(pos.p)) {
+            if (visited.contains(pos)) {
+                return true
+            }
+            visited.add(pos)
+            pos = calcNext(pos)
+        }
+        return false
+    }
+}
 
 fun parseFloorMap(lines: List<String>): FloorMap {
-    val xdim = lines[0].length
-    val ydim = lines.size
-    val guardChars = arrayOf<Char>(
-        '<', 'v', '>', '^'
-    )
-    var obsts = mutableListOf<Point>()
+    val xlim = lines[0].length
+    val ylim = lines.size
+    val guardChars = setOf('<', 'v', '>', '^')
+    val obsts = mutableListOf<Point>()
     var dir = Dir.S
-    var start = Point(0, 0) 
+    var start = Point(0, 0)
+
     lines.forEachIndexed { ycoord, l ->
         l.forEachIndexed { xcoord, c ->
-        	if (guardChars.contains(c)) {
-                start = Point(xcoord, ycoord)
-            }
-        	when(c) {
-                '<' -> dir = Dir.W
-                'v' -> dir = Dir.S
-                '>' -> dir = Dir.E
-                '^' -> dir = Dir.N
-                '#' -> obsts.add(Point(xcoord, ycoord))
+            when {
+                guardChars.contains(c) -> {
+                    start = Point(xcoord, ycoord)
+                    dir = when(c) {
+                        '<' -> Dir.W
+                        'v' -> Dir.S
+                        '>' -> Dir.E
+                        '^' -> Dir.N
+                        else -> throw IllegalArgumentException(
+                            "Should never happen $c"
+                        )
+                    }
+                }
+                c == '#' -> obsts.add(Point(xcoord, ycoord))
             }
         }
     }
-    return FloorMap(
-        xdim, ydim,
-        obsts, Vector(start, dir)
-    )
-}
-
-fun FloorMap.isInside(p: Point): Boolean {
-    return (p.x in 0 until xhi)
-    	&& (p.y in 0 until yhi)
-}
-
-fun FloorMap.print(visited: Set<Vector> = emptySet()) {
-    for (y in 0 until yhi) {
-        for (x in 0 until xhi) {
-            val p = Point(x, y)
-            if (obsts.contains(p)) {
-                print('#')
-            } else if(guy.p == p) {
-                print('S')
-            } else if(visited.any{it.p == p}) {
-                print('*')
-            } else {
-                print('.')
-            }
-        }
-        println()
-    }
+    return FloorMap(xlim, ylim, obsts, Vector(start, dir))
 }
 
 fun Vector.step(): Vector {
-   var x = p.x
-   var y = p.y
-   when (d) {
-       Dir.N -> y--
-       Dir.S -> y++
-       Dir.W -> x--
-       Dir.E -> x++
-   }
-   return Vector(Point(x, y), d)
-}
-
-// First AoC type extension!
-fun FloorMap.calcNext(v: Vector): Vector {
-    val n = v.step()
-    if (obsts.contains(n.p)) {
-       return Vector(v.p, v.d.turn()).step()
+    val (x, y) = when (d) {
+        Dir.N -> p.x to p.y - 1
+        Dir.S -> p.x to p.y + 1
+        Dir.W -> p.x - 1 to p.y
+        Dir.E -> p.x + 1 to p.y
     }
-    return n
+    return Vector(Point(x, y), d)
 }
-
-//fun runBoyRun()
-
 
 fun d06_1(f: String): Int {
-    val lines = File(f).readLines()
-    val fm = parseFloorMap(lines)
-    var next = fm.guy
-    var visited = mutableSetOf<Point>(next.p)
+    val fm = parseFloorMap(File(f).readLines())
+    var next = fm.start
+    val visited = mutableSetOf(next.p)
+    
     while (fm.isInside(next.p)) {
         visited.add(next.p)
         next = fm.calcNext(next)
@@ -110,46 +119,45 @@ fun d06_1(f: String): Int {
     return visited.size
 }
 
-fun FloorMap.isLoopy(visit: Set<Vector>): Boolean {
-    var next = guy
-    var visited = visit.toMutableSet()
-    while (isInside(next.p)) {
-        if (visited.contains(next)) {
-            return true
-        }
-        visited.add(next)
-        next = calcNext(next)
-    }
-    return false
-}
-
 fun d06_2(f: String): Int {
-    val lines = File(f).readLines()
-    val fm = parseFloorMap(lines)
-    //fm.print()
-    var pos = fm.guy
-    var visited = mutableSetOf<Vector>()
-    var loopers = mutableSetOf<Point>()
+    println("Init ${LocalTime.now()}")
+    val fm = parseFloorMap(File(f).readLines())
+    var pos = fm.start
+    val visited = mutableSetOf<Vector>()
+    val loopers = mutableSetOf<Point>()
+    
     while (fm.isInside(pos.p)) {
         val next = fm.calcNext(pos)
-        // Not on the guy...
-        if (next.p == fm.guy.p) {
+        if (next.p == fm.start.p || visited.contains(next)) {
             visited.add(pos)
             pos = next
             continue
         }
-        // add obstacle @next
-        // is modified loopy?
-        // continue on unaltered map
-        val obstsMod: List<Point> = fm.obsts.plus(next.p)
-        val fmMod = FloorMap(fm.xhi, fm.yhi, obstsMod, fm.guy)
-        if (fmMod.isLoopy(emptySet())) {
+        
+        val modifiedMap = FloorMap(
+            fm.xlim,
+            fm.ylim,
+            fm.obsts + next.p,
+            fm.start
+        )
+        
+        if (modifiedMap.isLoopy()) {
             loopers.add(next.p)
-            println("Loops with $next")
-            fmMod.print(visited)
         }
         visited.add(pos)
         pos = next
     }
+    println("Fini ${LocalTime.now()}")
     return loopers.size
+}
+
+
+fun main() {
+    try {
+        println(d06_1("input"))
+        println(d06_2("input"))
+        // 1696 1563 nope
+    } catch (ex: Exception) {
+        println("error: ${ex.message}")
+    }
 }
